98bca4d70f6ad5359a496abe008f658a50318f96
Ab7adi <mahmod.hadi19@gmail.com>
2024-07-17 10:49:19 +0300

    Update Client

--- AppClientSide/App.jsx
+++ AppClientSide/App.jsx
@@ -1,16 +1,27 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'react-native';
 
-
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-   <PaperProvider>
-    <NavigationContainer>
-
-    </NavigationContainer>
-   </PaperProvider>
+    <SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <PaperProvider>
+      <StatusBar barStyle="dark-content" />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Screen2">
+           
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </GestureHandlerRootView>
+  </SafeAreaProvider>
   );
 }
 
